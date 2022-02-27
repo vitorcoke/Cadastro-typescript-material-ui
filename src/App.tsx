d9d70856955aaa4a3572dd8a2a8e2836143d0330
vitorcoke <cokevitor@gmail.com>
2022-02-27 20:46:02 -0300

    feita algumas correções de erro

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes";
-import { AppThemeProvider } from "./shared/contexts";
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './routes';
+import { AppThemeProvider } from './shared/contexts';
 
 
 function App() {
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,15 @@
-import { Button } from "@mui/material";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useAppThemeContext } from "../shared/contexts";
+import { Button } from '@mui/material';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAppThemeContext } from '../shared/contexts';
 
 const AppRoutes = () => {
-    const { toogleTheme } = useAppThemeContext()
-    return (
-        <Routes>
-            <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toogleTheme}>Teste</Button>} />
-            <Route path="*" element={<Navigate to='/pagina-inicial' />} />
-        </Routes>
-    )
-}
+  const { toogleTheme } = useAppThemeContext();
+  return (
+    <Routes>
+      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toogleTheme}>Teste</Button>}/>
+      <Route path="*" element={<Navigate to='/pagina-inicial' />} />
+    </Routes>
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;

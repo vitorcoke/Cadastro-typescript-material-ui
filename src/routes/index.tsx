4ea10724a027c975011b8fa122e8b64f76a1783b
vitorcoke <cokevitor@gmail.com>
2022-02-27 21:01:56 -0300

    feat: erro de arquivos, copiado novamente

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,15 @@
-import { Button } from "@mui/material";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Button } from '@mui/material';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAppThemeContext } from '../shared/contexts';
 
 const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/pagina-inicial" element={<Button>Teste</Button>} />
-            <Route path="*" element={<Navigate to='/pagina-inicial'/>} />
-        </Routes>
-    )
-}
+  const { toogleTheme } = useAppThemeContext();
+  return (
+    <Routes>
+      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toogleTheme}>Teste</Button>}/>
+      <Route path="*" element={<Navigate to='/pagina-inicial' />} />
+    </Routes>
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,23 @@
+import { createTheme } from '@mui/material';
+import { cyan, yellow } from '@mui/material/colors';
+
+export const LightTheme = createTheme({
+  palette: {
+    primary: {
+      main: yellow[700],
+      dark: yellow[600],
+      light: yellow[500],
+      contrastText: '#fff'
+    },
+    secondary: {
+      main: cyan[700],
+      dark: cyan[600],
+      light: cyan[500],
+      contrastText: '#fff'
+    },
+    background: {
+      default: '#f7f8f3',
+      paper: '#fff'
+    }
+  }
+});
